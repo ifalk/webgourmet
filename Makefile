ffc2614f5a916b9fb3d7327cddd409cfab4fbd8f
Ingrid Falk <ingrid.falk@posteo.eu>
2023-06-05 15:55:55 +0200

    produce json file for online recipe search

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@
 
 SCRIPTS_PL=scripts
 
-RECIPES_DB=/home/falk/webgourmet/tests/recipes.db
+#RECIPES_DB=/home/falk/webgourmet/tests/recipes.db
+RECIPES_DB=/home/falk/webgourmet/recipes_202306.db
 
 JS_CSS=/home/falk/webgourmet/web
 
@@ -47,16 +48,20 @@ index.html: $(SCRIPTS_PL)/make_html_index.pl id2file_name.json
 ## Please ensure that db is not locked when calling
 # recipes.json: $(SCRIPTS_PL)/db2json.pl id_2_html_links.pl $(RECIPES_DB)
 # 	perl $< --links=./id_2_html_links.pl $(RECIPES_DB) > $@
+# recipes.json: $(SCRIPTS_PL)/db2json.pl $(RECIPES_DB)
+# 	perl $< $(RECIPES_DB) > $@
 
-recipes.json: $(SCRIPTS_PL)/db2json.pl $(RECIPES_DB)
-	perl $< $(RECIPES_DB) > $@
+### generate json array from hashes extracted and stored from gourmet sqlite db
+recipes.json: $(SCRIPTS_PL)/make_json_for_search.pl recipe_hash.json ingredient_hash.json id2file_name.json
+	$(info Make: *** Producing recipes.json needed for search ***)
+	-perl $< --recipe_json=recipe_hash.json --ingredient_json=ingredient_hash.json --id2file_name_json=id2file_name.json > $@
 
-
+### we no longer need this (2023-06)
 # DB_LAST_MOD := $(shell stat -c %y $$RECIPES_DB | cut -d" " -f1)
-DB_LAST_MOD := $(shell echo $$RECIPES_DB)
-show_last_db_mod: $(RECIPES_DB)
-	DB_LAST_MOD=$$(stat -c %y $(RECIPES_DB) | cut -d" " -f1) ;\
-	echo $$DB_LAST_MOD
+# DB_LAST_MOD := $(shell echo $$RECIPES_DB)
+# show_last_db_mod: $(RECIPES_DB)
+# 	DB_LAST_MOD=$$(stat -c %y $(RECIPES_DB) | cut -d" " -f1) ;\
+# 	echo $$DB_LAST_MOD
 
 ### copy files to apache root for test
 clean_local: 
@@ -68,7 +73,7 @@ clean_local:
 	sudo rm -f *.css && \
 	sudo rm -f *.js
 
-copy_local: clean_local #recipes.json index.html
+copy_local: clean_local recipes.json index.html
 	sudo cp $(HTML_LOCAL)/*.htm* $(WWW_LOCAL)/ && \
 	sudo cp -r $(HTML_LOCAL)/pics $(WWW_LOCAL)/ && \
 	sudo cp $(HTML_LOCAL)/style.css $(WWW_LOCAL)/ && \
--- scripts/make_json_for_search.pl
+++ scripts/make_json_for_search.pl
@@ -1,18 +1,43 @@
 use lib '/home/falk/webgourmet/scripts';
 use GourmetExport;
 use Data::Dumper;
-use Test::More qw( no_plan );
-
-use DBI;
-use DBD::SQLite::Constants qw/:file_open/;
 
 use JSON::XS qw(encode_json decode_json);
 use File::Slurp qw(read_file write_file);
 
-my $recipe_json_file_name = 'recipe_hash.json';
-my $ingredient_json_file_name = 'ingredient_hash.json';
-my $id2file_name_json_file_name = 'id2file_name.json';
+use Getopt::Long 'HelpMessage';
 
+GetOptions(
+  'recipe_json=s' => \(my $recipe_json_file_name = 'recipe_hash.json'),
+  'ingredient_json=s' => \(my $ingredient_json_file_name = 'ingredient_hash.json'),
+  'id2file_name_json=s' => \(my $id2file_name_json_file_name = 'id2file_name.json'),
+  ) or HelpMessage(1);
+
+
+=head1 NAME
+
+make_json_for_search - build json array which is needed for web search.
+
+For this we use data extracted from the gourmet db and stored in 3 (json) hashes:
+
+- recipe_hash     contains data related to recipes
+- ingredient_hash contains data about ingredients
+- id2file_name    contains data for html index, i.e. among others links between titles, html files, ids, and categories 
+
+=head1 SYNOPSIS
+
+  --recipe_json     name of the file where recipe data is stored, defaults to recipe_hash.json
+  --ingredient_json name of the file where ingredient data is stored, defaults to ingredient_hash.json
+  --id2file_name    name of file where html index data is stored, defaults to id2file_name.json
+  --help,-h         Print this help
+
+The resulting json array is written to STDOUT.
+
+=head1 VERSION
+
+0.01
+
+=cut
 
 my $json = read_file($recipe_json_file_name, { binmode => ':raw' });
 my $recipe_hash = decode_json($json);
@@ -25,11 +50,9 @@ my $id2file_name = decode_json($json);
 
 my @recipes_json;
 
-# foreach my $id (sort {$a <=> $b} (keys %{ $recipe_hash })[0..20]) {
 foreach my $id (sort {$a <=> $b} keys %{ $recipe_hash }) {
   next unless (exists $id2file_name->{$id} and exists $id2file_name->{$id}->{'html_file_name'});
 
-  ### do we need this?
   my $linkrecipe = $id2file_name->{$id}->{'html_file_name'};
   my $title = $recipe_hash->{$id}->{'title'};
   my $title_string = "<a href='$linkrecipe' target='_blank'>$title</a>";
@@ -69,4 +92,5 @@ foreach my $id (sort {$a <=> $b} keys %{ $recipe_hash }) {
 }
 
 $json = encode_json(\@recipes_json);
-write_file('recipes_test.json', { binmode => ':raw' }, $json)
+write_file(\*STDOUT, $json);
+  
